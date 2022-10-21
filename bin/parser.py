f2c723b2bc65e60c3f94d9ef4b977e7edcf4185f
Jarrus00 <Jarret.Russell@WSU.edu>
2022-10-20 21:53:12 -0700

    Moving to ./bin and implemented neo4j driver
    Move will group this with other scripts/executables/etc.
    Very basic neo4j driver setup for creating nodes w/properties.
    Now depends on ./etc/config.ini to supply db parameters.

--- bin/parser.py
+++ bin/parser.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python3
+
+import os
+import re
+import configparser as cfg
+from neo4j import GraphDatabase as gdb
+
+project_root = re.sub('(?<=Amazon-CoPurchasing).*', '', os.path.abspath('.'))
+config_path = os.path.join(project_root, 'etc', 'config.ini')
+
+config = cfg.ConfigParser()
+config.read(config_path)
+
 def load(filename):
     # open dataset fiel to read
     data=dict()
@@ -66,10 +79,46 @@ def similartocsv(similardata, csvfile):
         for product in similardata:
             csv.write(str(product[0]) + ',' + str(product[1]) + '\n')
 
+
+# Methods for loading data into neo4j db (JR)
+class N4J:
+    def __init__(self):
+        self.endpoint = ''.join(['bolt://', config.get('database_connection', 'dbhost'), ':', config.get('database_connection', 'dbport')])
+        self.driver = gdb.driver(
+            self.endpoint,
+            auth=(config.get('database_connection', 'dbuser'), config.get('database_connection', 'dbpass'))
+        )
+
+    def close(self):
+        self.driver.close()
+
+    def add_node(self, idx, node_data):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._create_acp_n4_node, idx, node_data)
+            print(result) # Switch to log? (JR)
+
+    @staticmethod
+    def _create_acp_n4_node(transaction, idx, node_data):
+        # Combine values by unpacking to get interpolation to work without additional string processing - merge operator ('|') only exists for Python 3.9+ (JR)
+        # Need to prepend with a character since neo4j does not allow for node names to start with numbers: https://neo4j.com/docs/cypher-manual/current/syntax/naming/ (JR)
+        # The node name can easily be replaced with another value, this is just to prototype the process before the data model is ready (JR)
+        cypher = 'CREATE (%(idx)s:Product {ASIN:\'%(ASIN)s\', title:\'%(title)s\', group:\'%(group)s\', salesrank:\'%(salesrank)s\'})' % {**{'idx':'a_' + idx}, **node_data}
+        result = transaction.run(cypher)
+        return
+
+
 def main():
 
     #dict of raw data
-    dataset = load("sample.txt")
+    # dataset = load("sample.txt")
+    # Ensuring that the path is constructed agnostic to the system which runs this (JR)
+    dataset = load(os.path.join(project_root, 'data', 'amazon-meta_first25.txt'))
+
+    # Testing sending over the full dataset after extraction instead of inline (may incur additional overhead, need to test) (JR)
+    n4_loader = N4J()
+    [n4_loader.add_node(x, dataset[x]) for x in dataset]
+    n4_loader.close()
+
     # edges
     adjset = returnsimilar(dataset)
     
