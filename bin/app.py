a0dfc3d975c15d94831258c146d3f59f8e1cf1b2
Jarrus00 <Jarret.Russell@WSU.edu>
2022-12-09 19:35:57 -0800

    Added reset nuances, changed query output to table

--- bin/app.py
+++ bin/app.py
@@ -45,26 +45,57 @@ class AcpApp(QMainWindow):
         self.ui.setupUi(self)
         self.loadList()
         self.ui.listWidget.itemClicked.connect(self.Clicked1)
+        self.ui.listWidget.itemSelectionChanged.connect(self.node_source_changed)
         self.ui.listWidget_2.itemClicked.connect(self.Clicked2)
+        self.ui.listWidget_2.itemSelectionChanged.connect(self.property_key_changed)
         self.ui.listWidget_3.itemClicked.connect(self.Clicked3)
+        self.ui.listWidget_3.itemSelectionChanged.connect(self.condition_op_changed)
         self.ui.pushButton.clicked.connect(self.Clicked4)
         self.ui.btn_gen_cf_recs.clicked.connect(self.btn_gen_cf_recs_clicked)
+        self.ui.btn_reset.clicked.connect(self.reset_ui)
         self.products = dict()
         self.n4 = N4J()
 
     def loadList(self):
-                self.ui.listWidget.clear()
-                self.ui.listWidget.addItem('PRODUCT')
-                self.ui.listWidget.addItem('CATEGORY')
-                self.ui.listWidget.addItem('CUSTOMER')
-                self.ui.listWidget.addItem('REVIEW')
-                self.ui.listWidget_3.addItem('<')
-                self.ui.listWidget_3.addItem('<=')
-                self.ui.listWidget_3.addItem('=')
-                self.ui.listWidget_3.addItem('>=')
-                self.ui.listWidget_3.addItem('>')                
-            
-                
+        self.ui.listWidget.clear()
+        self.ui.listWidget.addItem('PRODUCT')
+        self.ui.listWidget.addItem('CATEGORY')
+        self.ui.listWidget.addItem('CUSTOMER')
+        self.ui.listWidget.addItem('REVIEW')
+        self.ui.listWidget_3.addItem('<')
+        self.ui.listWidget_3.addItem('<=')
+        self.ui.listWidget_3.addItem('=')
+        self.ui.listWidget_3.addItem('>=')
+        self.ui.listWidget_3.addItem('>')
+    
+    def style_query_results_table(self, data_dims, header_vals):
+        # Expects to receive a two-value tuple in the form (row_count, column_count) (JR)
+        self.ui.tbl_query_results.horizontalHeader().setFixedHeight(50)
+        self.ui.tbl_query_results.setColumnCount(data_dims[1])
+        self.ui.tbl_query_results.setRowCount(data_dims[0])
+        self.ui.tbl_query_results.setHorizontalHeaderLabels(header_vals)
+        self.ui.tbl_query_results.setColumnWidth(0, int(round(self.ui.tbl_query_results.width() * 0.25, 0)))
+        self.ui.tbl_query_results.setColumnWidth(1, self.ui.tbl_query_results.width() - self.ui.tbl_query_results.columnWidth(0))
+        self.ui.tbl_query_results.horizontalHeader().setDefaultAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.Alignment(QtCore.Qt.TextWordWrap))
+    
+    def reset_query_results_table(self):
+        for i in reversed(range(self.ui.tbl_query_results.rowCount())):
+            self.ui.tbl_query_results.removeRow(i)
+
+    def reset_cf_results_table(self):
+        for i in reversed(range(self.tbl_cf_recs.rowCount())):
+            self.ui.tbl_cf_recs.removeRow(i)
+
+    def reset_ui(self):
+        # Empty out downstream elements (JR)
+        self.ui.listWidget.clearSelection()
+        self.ui.listWidget_2.clear()
+        self.ui.textEdit.clear()
+        # self.ui.textEdit_2.clear()
+        self.ui.lst_cf_recs.clear()
+        self.reset_query_results_table()
+        self.reset_cf_results_table()
+        
 
         #DEMO CODE NOT NECESSARY FOR NOW
                 # try:
@@ -129,10 +160,10 @@ class AcpApp(QMainWindow):
 
         self.gridLayout.addWidget(self.pushButton, 3, 1, 1, 2)
 
-        self.textEdit_2 = QTextEdit(self.centralwidget)
-        self.textEdit_2.setObjectName(u"textEdit_2")
+        # self.textEdit_2 = QTextEdit(self.centralwidget)
+        # self.textEdit_2.setObjectName(u"textEdit_2")
 
-        self.gridLayout.addWidget(self.textEdit_2, 4, 0, 1, 4)
+        # self.gridLayout.addWidget(self.textEdit_2, 4, 0, 1, 4)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -194,32 +225,70 @@ class AcpApp(QMainWindow):
 
     def Clicked4(self):
         n=N4J()
-        self.ui.textEdit_2.clear()
+        # self.ui.textEdit_2.clear()
 
         global numeric_val
         numeric_val = self.ui.textEdit.toPlainText()
       
         self.products = n.get_rating_greater(rating=numeric_val,operand=condition)
-        self.ui.textEdit_2.append(str(self.products))
+        # self.ui.textEdit_2.append(str(self.products.values))
+
+        self.style_query_results_table(self.products.shape, list(self.products.columns))
+
+        for row_idx in range(self.products.shape[0]):
+            for col_idx in range(0, self.products.shape[1]):
+                self.ui.tbl_query_results.setItem(row_idx, col_idx, QTableWidgetItem(str(self.products.values[row_idx, col_idx])))
+
     # def Clicked3(self,item):
 	#     QMessageBox.information(self, "ListWidget", "You clicked: "+item.text())
 
     #def populate_List_2(self,MainWindow):
 
+    def node_source_changed(self):
+        # Empty out downstream elements (JR)
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget_3.clearSelection()
+        self.ui.textEdit.clear()
+        # self.ui.textEdit_2.clear()
+        self.ui.lst_cf_recs.clear()
+        self.reset_query_results_table()
+        self.reset_cf_results_table()
+
+    def property_key_changed(self):
+        # Empty out downstream elements (JR)
+        self.ui.listWidget_3.clearSelection()
+        self.ui.textEdit.clear()
+        # self.ui.textEdit_2.clear()
+        self.ui.lst_cf_recs.clear()
+        self.reset_query_results_table()
+        self.reset_cf_results_table()
+
+    def condition_op_changed(self):
+        # Empty out downstream elements (JR)
+        self.ui.textEdit.clear()
+        # self.ui.textEdit_2.clear()
+        self.ui.lst_cf_recs.clear()
+        self.reset_query_results_table()
+        self.reset_cf_results_table()
+
+
     def btn_gen_cf_recs_clicked(self):
+        # Generate list of recommendations based on the subset returned to the UI (JR)
         self.ui.lst_cf_recs.clear()
 
-        wtd_mtx = self.n4.get_cf_set_from_asins(list(set(x['asin'] for x in self.products)))
-        cid = rnd.sample(list(wtd_mtx.columns.values), 1)[0]
+        if len(self.products) == 0:
+            self.ui.lst_cf_recs.addItem('No products to derive recommendations from.')
+        
+        else:
+            wtd_mtx = self.n4.get_cf_set_from_asins(list(set(x['asin'] for x in self.products)))
+            cid = rnd.sample(list(wtd_mtx.columns.values), 1)[0]
 
-        cf = CollaborativeFilter(wtd_mtx, cid)
-        recs = cf.recommend_product(cid, 5)
-        rec_titles = self.n4.get_titles_from_asins(recs)
+            cf = CollaborativeFilter(wtd_mtx, cid)
+            recs = cf.recommend_product(cid, 5)
+            rec_titles = self.n4.get_titles_from_asins(recs)
 
-        for row in rec_titles:
-            self.ui.lst_cf_recs.addItem(row)
-
-        return
+            for row in rec_titles:
+                self.ui.lst_cf_recs.addItem(row)
        
 
 
